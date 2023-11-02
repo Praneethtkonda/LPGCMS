21bc537b7d0996fab3435dbbf3235aeb46421836
praneethtkonda <praneethtkonda@gmail.com>
2023-11-02 23:20:00 +0530

    Android initial code with splash screen

---
+++ android/app/src/main/java/com/lphomes/lpgcms/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.lphomes.lpgcms
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lphomes.lpgcms.ui.theme.LpgcmsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
